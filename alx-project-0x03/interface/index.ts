308d723dffa5d7e3504434805cba55af0253e98c
Njeri Kamau <faithkamau730@gmail.com>
2025-06-08 22:27:27 +0300

    Interface are in the right directory

---
+++ alx-project-0x03/interface/index.ts
@@ -0,0 +1,17 @@
+import { ReactNode } from 'react';
+
+export interface PageRouteProps {
+  pageRoute: string
+}
+
+export interface LayoutProps {
+  children: ReactNode;
+}
+
+export interface ButtonProps {
+  buttonLabel: string
+  buttonSize?: string
+  buttonBackgroundColor?: 'red' | 'blue' | 'orange' | 'green'
+  action?: () => void
+}
+
